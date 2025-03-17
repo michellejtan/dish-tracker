e78874d182e7b9adb99df898547a71fc5bee27fb
michellejtan <michelle0223@gmail.com>
2025-03-17 16:40:00 -0700

    wip: Create the DIsh schema & Embed dishSchema in userSchema

--- models/user.js
+++ models/user.js
@@ -1,14 +1,46 @@
 const mongoose = require('mongoose');
 
+// Define the Dish schema
+const dishSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  restaurantName: {
+    type: String,
+    required: true
+  },
+  restaurantLocation: {
+    type: String
+  },
+  comments: {
+    type: [String],
+    default: []
+  },  // Array of comments
+  status: {
+    type: String,
+    enum: ['Tried', 'Want to Try'],
+    default: 'Want to Try'
+  },
+  websiteLink: {
+    type: String
+  },
+},
+  {
+    timestamps: true
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
   },
+  name: { type: String },
   password: {
     type: String,
     required: true,
   },
+  dishes: [dishSchema]  // Embedded dishes array
 });
 
 const User = mongoose.model('User', userSchema);
