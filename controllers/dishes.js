b3324ed3630d49037f7fc202e0d6e790fa37d476
michellejtan <michelle0223@gmail.com>
2025-03-19 01:52:38 -0700

    wip: redirect to foods index & render template & & complete create functionality

--- controllers/dishes.js
+++ controllers/dishes.js
@@ -3,5 +3,34 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// GET /users/:userId/dishes
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.locals.dishes = currentUser.dishes;
+        res.render('dishes/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// GET /users/:userId/dishes/new
+router.get('/new', async (req, res) => {
+    res.render('dishes/new.ejs');
+});
+
+// POST /users/:userId/dishes
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.dishes.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/dishes`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
